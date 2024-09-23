4263adf573b502b2e3bcdefbe444b886574709fe
bmaupin <bmaupin@users.noreply.github.com>
2024-09-23 14:00:57 -0400

    feat: Start the next batch

--- scripts/cleanup-html.ts
+++ scripts/cleanup-html.ts
@@ -24,24 +24,29 @@ const blacklistSelectorsBeforeCustomLogic: string[] = [
   'style',
   'script',
   'header',
-  'div.mainContent svg.icon.hollow',
-  'div.mainContent div.noCarousel div.video-container',
-  'div.mainContent #carousel div.video-container',
-  'div.mainContent #carousel div.moreVideos',
-  'div.mainContent div.textBelow',
-  'div.mainContent div.stages:has(div.spacer)',
-  'div.mainContent .ng-hide',
-  'div.mainContent .viewToggle',
-  'div.mainContent .expansionLink',
-  'div.mainContent .gif-player .loading',
-  'div.mainContent .gif-player .preview',
-  'div.mainContent .gif-player .stop',
-  'div.mainContent div.expansionBlock div.blockVid',
-  'div.mainContent div.expansionBlock span.blockVid',
-  'div.mainContent div.expansionBlock div.blockTouch',
-  'div.mainContent div.expansionBlock svg.icon-arrow-thin',
-  'div.mainContent div.expandedContent span.blockVid',
-  'div.mainContent div.quoteBlock .m7',
+  '.hint-popups',
+  '.technique-side-nav',
+  '.technique-top-nav',
+  'div.main-content svg[class*="-icon"]',
+  'div.main-content .carousel #carousel',
+  'div.main-content .carousel .top-video__carousel-block',
+  'div.main-content .basic-video-block',
+  'div.main-content .quote-slider__quote.slick-cloned',
+  'div.main-content .slider-controls',
+  'div.main-content .gif-player .gif-ctrl',
+
+  'div.main-content .feedback-banner',
+
+  // 'div.mainContent div.noCarousel div.video-container',
+  // 'div.mainContent div.textBelow',
+  // 'div.mainContent div.stages:has(div.spacer)',
+  // 'div.mainContent .ng-hide',
+  // 'div.mainContent .viewToggle',
+  // 'div.mainContent .expansionLink',
+  // 'div.mainContent div.expansionBlock div.blockTouch',
+  // 'div.mainContent div.expansionBlock svg.icon-arrow-thin',
+  // 'div.mainContent div.expandedContent span.blockVid',
+  // 'div.mainContent div.quoteBlock .m7',
   'div[onload]',
   'footer',
 ];
@@ -57,7 +62,8 @@ const whitelistSelectors: string[] = [
 // Add any extra site-specific logic to apply here
 const applyCustomLogic = (document: Document) => {
   // Override HTML title
-  document.title = document.querySelector('h1.pageTitle')?.textContent ?? '';
+  document.title =
+    document.querySelector('h1.page-heading__title')?.textContent ?? '';
 
   // Remove external links
   const anchorElements = document.querySelectorAll('a');
@@ -77,65 +83,16 @@ const applyCustomLogic = (document: Document) => {
   }
 
   // Conditionally remove some elements if they exist at same time as other elements
-  const copySmElements = document.querySelectorAll('div.mainContent p.copy.sm');
-  const copyM6Elements = document.querySelectorAll('div.mainContent p.copy.m6');
-  if (copySmElements.length > 0 && copyM6Elements.length > 0) {
-    copySmElements.forEach((element) => {
-      element.remove();
-    });
-  }
-  const topSmElements = document.querySelectorAll('div.mainContent div.top.sm');
-  const topXlElements = document.querySelectorAll('div.mainContent div.top.xl');
-  if (topSmElements.length > 0 && topXlElements.length > 0) {
-    topSmElements.forEach((element) => {
-      element.remove();
-    });
-  }
-  const statisticAfterElements = document.querySelectorAll(
-    'div.mainContent span.statistic.after'
-  );
-  const statisticBeforeElements = document.querySelectorAll(
-    'div.mainContent span.statistic.before'
-  );
-  if (statisticAfterElements.length > 0 && statisticBeforeElements.length > 0) {
-    statisticAfterElements.forEach((element) => {
-      element.remove();
-    });
-  }
   const gifMovieElements = document.querySelectorAll(
-    'div.mainContent .gif-player .gif-movie'
+    'div.main-content .gif-player .gif-movie'
   );
   const gifStillElements = document.querySelectorAll(
-    'div.mainContent .gif-player .gif-still'
+    'div.main-content .gif-player .gif-still'
   );
   if (gifMovieElements.length > 0 && gifStillElements.length > 0) {
     gifMovieElements.forEach((element) => {
       element.remove();
     });
-  }
-
-  const divLazyBgImageElements = document.querySelectorAll(
-    'div.quoteBlock div[data-lazy-bg-image]'
-  );
-  for (const divLazyBgImageElement of divLazyBgImageElements) {
-    const h2Element = divLazyBgImageElement.querySelector('h2');
-    if (divLazyBgImageElement && h2Element) {
-      const blockquote = document.createElement('blockquote');
-      blockquote.textContent = h2Element.textContent;
-      divLazyBgImageElement.replaceWith(blockquote);
-    }
-  }
-
-  // Insert a horizontal row if an element has a 'separator' class
-  const elementsWithSeparator = document.querySelectorAll(
-    'div.mainContent .separator'
-  );
-  for (const elementWithSeparator of elementsWithSeparator) {
-    const hr = document.createElement('hr');
-    elementWithSeparator.parentNode?.insertBefore(
-      hr,
-      elementWithSeparator.nextSibling
-    );
   }
 
   // Remove all existing h1 elements to prepare for the next section
